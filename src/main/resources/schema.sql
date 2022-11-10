3362d71d6f223df1dc87c313c237537323d36cfa
walidbousnina@gmail.com <abnejib23@gmail.com>
2022-11-10 10:36:44 +0100

    bytea change

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,15 +1,15 @@
 create table if not exists oauth_access_token (
   token_id VARCHAR(255),
-  token LONG VARBINARY,
+  token LONG bytea,
   authentication_id VARCHAR(255) PRIMARY KEY,
   user_name VARCHAR(255),
   client_id VARCHAR(255),
-  authentication LONG VARBINARY,
+  authentication LONG bytea,
   refresh_token VARCHAR(255)
 );
 
 create table if not exists oauth_refresh_token (
   token_id VARCHAR(255),
-  token LONG VARBINARY,
-  authentication LONG VARBINARY
+  token LONG bytea,
+  authentication LONG bytea
 );
