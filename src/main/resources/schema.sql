d2c7b65c501e86df42523862abf0a84909df7f6d
walidbousnina@gmail.com <abnejib23@gmail.com>
2022-11-10 12:56:26 +0100

    minimize data show mysql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,15 +1,15 @@
 create table if not exists oauth_access_token (
     token_id VARCHAR(255),
-    token BYTEA ,
+    token LONG VARBINARY,
     authentication_id VARCHAR(255) PRIMARY KEY,
     user_name VARCHAR(255),
     client_id VARCHAR(255),
-    authentication BYTEA ,
+    authentication LONG VARBINARY,
     refresh_token VARCHAR(255)
     );
 
 create table if not exists oauth_refresh_token (
     token_id VARCHAR(255),
-    token BYTEA ,
-    authentication BYTEA
+    token LONG VARBINARY,
+    authentication LONG VARBINARY
     );
